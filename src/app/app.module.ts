55e5666394203c01f6c4ea80174732227cdc46f1
Nguyen Kim Hiep <nkhiep@tma.com.vn>
2019-04-19 11:11:17 +0700

    updarte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,7 @@ import { AppRoutingModule } from './app.routing';
 // Import 3rd party components
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { ChartsModule } from 'ng2-charts';
 import { ApiService } from './services/api.service';
 import { AuthService } from './services/auth.service';
 import { AuthGuard } from './auth.guard';
